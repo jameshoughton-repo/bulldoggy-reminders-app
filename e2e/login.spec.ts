90ddcf2160b72f4341d4af013f306f28ac33ff70
James Houghton <43765601+jameshoughton-repo@users.noreply.github.com>
2025-04-16 15:17:37 +0100

    Update login.spec.ts
    Addition of tests 
    - invalid account details 
    - login page validation

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,9 +1,24 @@
 import { test, expect } from '@playwright/test';
 
+// login with standard user account 
 test('test login | standard user', async ({ page }) => {
   await page.goto('http://127.0.0.1:8000/login');
   await page.getByRole('textbox', { name: 'Enter username' }).fill('pythonista');
   await page.getByRole('textbox', { name: 'Enter password'}).fill('I<3testing');
   await page.getByTestId('login-button').click();
   await expect(page.getByText('Reminders for pythonista')).toBeVisible();
+});
+
+// login with invalid account details 
+test('test login | invalid user', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/login');
+  await page.getByRole('textbox', { name: 'Enter username' }).fill('test');
+  await page.getByRole('textbox', { name: 'Enter password'}).fill('test');
+  await page.getByTestId('login-button').click();
+});
+
+// login page validation
+test('test login | field validation', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/login');
+  await page.getByTestId('login-button').click();
 });
