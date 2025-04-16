870cd921b526d5a9557303a96890facde81ececa
James Houghton <jameshoughton0123@gmail.com>
2025-04-16 17:11:23 +0100

    Cleanup | adding variables for test data

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,6 +1,12 @@
 import { test } from '@playwright/test';
 import { LoginPage } from './pages/login-page';
 
+// test data 
+const validUsername = 'pythonista'; 
+const validPassword = 'I<3testing';
+const invalidUsername = 'test';
+const invalidPassword = 'test';
+
 // test setup
 test.beforeEach(async ({ page }) => {
   const loginPage = new LoginPage(page);
@@ -10,16 +16,16 @@ test.beforeEach(async ({ page }) => {
 // login with standard user account 
 test('test login | standard user', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  await loginPage.inputUsername('pythonista');
-  await loginPage.inputPassword('I<3testing');
+  await loginPage.inputUsername(validUsername);
+  await loginPage.inputPassword(validPassword);
   await loginPage.clickLogin();
 });
 
 // login with invalid account details 
 test('test login | invalid user', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  await loginPage.inputUsername('invalidUser');
-  await loginPage.inputPassword('invalidPassword');
+  await loginPage.inputUsername(invalidUsername);
+  await loginPage.inputPassword(invalidPassword);
   await loginPage.clickLogin();
   await loginPage.assertInvalidLogin();
 });
